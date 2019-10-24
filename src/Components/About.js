593e17601da9c87858ac31938b70c29c19522727
Shashi Mishra <shashilion2002@gmail.com>
2019-10-24 21:47:32 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <img
               className="profile-pic"
               src={profilepic}
-              alt="Krishank Dwivedi Profile Pic"
+              alt="Shashi Profile Pic"
             />
           </div>
           <div className="nine columns main-col">
